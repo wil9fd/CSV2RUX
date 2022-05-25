29d460c232f03ebda441a9973e629eadf51f600f
Williams, Samson (NCMI, Hobart) <Samson.Williams@csiro.au>
2022-05-25 17:09:07 +1000

    Yet another update

--- update.bat
+++ update.bat
@@ -1,5 +1,8 @@
+cmd /k
 call C:\Users\wil9fd\csv2rux_dev\venv_csv2rux\Scripts\activate.bat
 pyinstaller --clean -y CSV2RUX.spec
+call deactivate
+
 rmdir /s /q CSV2RUX.zip
 powershell Compress-Archive C:\Users\wil9fd\csv2rux_python_v1.0.0\dist\CSV2RUX\* C:\Users\wil9fd\csv2rux_python_v1.0.0\CSV2RUX.zip ^
 rmdir /s /q build
@@ -8,4 +11,3 @@ git add -A
 git commit -m "Yet another update" --all 
 git push ghub 
 git push bitbucket
-cmd /k
